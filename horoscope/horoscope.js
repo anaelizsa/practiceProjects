36ae6cf35255b3e2b6005035996ed4d4f4dc8247
Ana Elizondo-Salas <77133339+anaelizsa@users.noreply.github.com>
2025-02-18 20:50:22 -0600

    Add files via upload

---
+++ horoscope/horoscope.js
@@ -0,0 +1,29 @@
+let month = String(prompt('Insert Month (Capitalize first letter): '))
+
+if (month === 'January'){
+    console.log('Capricorn');
+} else if (month === 'February'){
+    console.log('Aquarius');
+} else if (month === 'March'){
+    console.log('Pisces');
+} else if (month === 'April'){
+    console.log('Aries');
+} else if (month === 'May'){
+    console.log('Taurus');
+} else if (month === 'June'){
+    console.log('Gemini');
+} else if (month === 'July'){
+    console.log('Cancer');
+} else if (month === 'August'){
+    console.log('Leo');
+} else if (month === 'September'){
+    console.log('Virgo');
+} else if (month === 'October'){
+    console.log('Libra');
+} else if (month === 'November'){
+    console.log('Scorpio');
+} else if (month === 'December'){
+    console.log('Sagittarius');
+} else {
+    console.log('Invalid Month, type month and capitalize first letter');
+}
